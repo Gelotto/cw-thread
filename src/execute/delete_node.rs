a5ee730c96646421e8dcc821fda0d562c8b849ce
Daniel Gabriele <dg0offset@gmail.com>
2023-12-24 17:17:57 -0500

    implemented delete

--- src/execute/delete_node.rs
+++ src/execute/delete_node.rs
@@ -1,5 +1,17 @@
-use crate::error::ContractError;
-use cosmwasm_std::{attr, Response};
+use crate::{
+    error::ContractError,
+    state::{
+        models::POSITIVE,
+        storage::{
+            CALLOUT_NODE_RELATIONSHIP, CHILD_RELATIONSHIP, HASHTAG_NODE_RELATIONSHIP,
+            NEG_REPLY_RELATIONSHIP, NODE_CALLOUT_RELATIONSHIP, NODE_ID_2_ATTACHMENT,
+            NODE_ID_2_BODY, NODE_ID_2_FLAG, NODE_ID_2_METADATA, NODE_ID_ADDR_2_SENTIMENT,
+            POS_REPLY_RELATIONSHIP,
+        },
+    },
+    util::load_node_metadata,
+};
+use cosmwasm_std::{attr, Addr, Order, Response};
 
 use super::Context;
 
@@ -8,10 +20,117 @@ pub fn exec_delete_node(
     id: u32,
 ) -> Result<Response, ContractError> {
     let Context { deps, .. } = ctx;
-    // TODO: update parent metadata
-    // TODO: remove body
-    // TODO: remove attachments
-    // TODO: remove tags
-    // TODO: remove callouts
+    let node = load_node_metadata(deps.storage, id, true)?.unwrap();
+
+    // Remove metadata
+    NODE_ID_2_METADATA.remove(deps.storage, id);
+
+    if let Some(parent_id) = node.reply_to_id {
+        // Remove child relationship
+        CHILD_RELATIONSHIP.remove(deps.storage, (parent_id, id));
+
+        // Update parent metadata
+        NODE_ID_2_METADATA.update(
+            deps.storage,
+            parent_id,
+            |maybe_parent| -> Result<_, ContractError> {
+                if let Some(mut parent) = maybe_parent {
+                    parent.n_replies -= 1;
+                    Ok(parent)
+                } else {
+                    Err(ContractError::NodeNotFound { node_id: parent_id })
+                }
+            },
+        )?;
+
+        // Remove from ranked reply ordering maps
+        if node.sentiment == POSITIVE {
+            POS_REPLY_RELATIONSHIP.remove(deps.storage, (parent_id, node.rank, id));
+        } else {
+            NEG_REPLY_RELATIONSHIP.remove(deps.storage, (parent_id, node.rank, id));
+        }
+    }
+
+    // Purge the node's sentiment state
+    {
+        let addrs: Vec<Addr> = NODE_ID_ADDR_2_SENTIMENT
+            .prefix(id)
+            .keys(deps.storage, None, None, Order::Ascending)
+            .map(|r| r.unwrap())
+            .collect();
+        for addr in addrs.iter() {
+            NODE_ID_ADDR_2_SENTIMENT.remove(deps.storage, (id, addr));
+        }
+    }
+
+    // Remove node body text
+    NODE_ID_2_BODY.remove(deps.storage, id);
+
+    // Remove flagged addresses with respect to the node
+    {
+        let addrs: Vec<Addr> = NODE_ID_2_FLAG
+            .prefix(id)
+            .keys(deps.storage, None, None, Order::Ascending)
+            .map(|r| r.unwrap())
+            .collect();
+        for addr in addrs.iter() {
+            NODE_ID_2_FLAG.remove(deps.storage, (id, addr));
+        }
+    }
+
+    // Remove attachments
+    for i in 0..node.n_attachments {
+        NODE_ID_2_ATTACHMENT.remove(deps.storage, (id, i));
+    }
+
+    // Remove callouts
+    {
+        let map = HASHTAG_NODE_RELATIONSHIP;
+        let keys: Vec<_> = map
+            .keys(deps.storage, None, None, Order::Ascending)
+            .map(|r| r.unwrap())
+            .collect();
+        for (a, b) in keys.iter() {
+            map.remove(deps.storage, (a, *b));
+        }
+    }
+    {
+        let map = CALLOUT_NODE_RELATIONSHIP;
+        let keys: Vec<_> = map
+            .keys(deps.storage, None, None, Order::Ascending)
+            .map(|r| r.unwrap())
+            .collect();
+        for (a, b) in keys.iter() {
+            map.remove(deps.storage, (a, *b));
+        }
+        for (a, b) in keys.iter() {
+            CALLOUT_NODE_RELATIONSHIP.remove(deps.storage, (a, *b));
+        }
+    }
+
+    // Remove tags
+    {
+        let map = NODE_CALLOUT_RELATIONSHIP;
+        let keys: Vec<_> = map
+            .keys(deps.storage, None, None, Order::Ascending)
+            .map(|r| r.unwrap())
+            .collect();
+
+        for (a, b) in keys.iter() {
+            map.remove(deps.storage, (*a, b));
+        }
+    }
+    {
+        let map = CALLOUT_NODE_RELATIONSHIP;
+        let keys: Vec<_> = map
+            .keys(deps.storage, None, None, Order::Ascending)
+            .map(|r| r.unwrap())
+            .collect();
+
+        for (a, b) in keys.iter() {
+            map.remove(deps.storage, (a, *b));
+        }
+    }
+
     Ok(Response::new().add_attributes(vec![attr("action", "delete")]))
 }
--- src/state/storage.rs
+++ src/state/storage.rs
@@ -7,16 +7,21 @@ use super::models::{Attachment, Config, FlagMetadata, NodeMetadata, TableInfo};
 pub const OWNER: Item<Owner> = Item::new("owner");
 pub const CONFIG: Item<Config> = Item::new("config");
 pub const TABLE: Item<TableInfo> = Item::new("table");
+
 pub const NODE_ID_COUNTER: Item<u32> = Item::new("node_id_counter");
 pub const NODE_ID_2_METADATA: Map<u32, NodeMetadata> = Map::new("node_id_2_metadata");
 pub const NODE_ID_2_BODY: Map<u32, String> = Map::new("node_id_2_body");
 pub const NODE_ID_2_ATTACHMENT: Map<(u32, u8), Attachment> = Map::new("node_id_2_attachment");
 pub const NODE_ID_2_FLAG: Map<(u32, &Addr), FlagMetadata> = Map::new("node_id_2_flag");
-pub const CHILD_RELATIONSHIP: Map<(u32, u32), bool> = Map::new("child_rel");
 pub const NODE_ID_ADDR_2_SENTIMENT: Map<(u32, &Addr), u8> = Map::new("node_id_addr_2_sentiment");
+
 pub const NEG_REPLY_RELATIONSHIP: Map<(u32, u32, u32), bool> = Map::new("neg_reply_rel");
 pub const POS_REPLY_RELATIONSHIP: Map<(u32, u32, u32), bool> = Map::new("pos_reply_rel");
-pub const HASHTAG_NODE_RELATIONSHIP: Map<(&String, u32), bool> = Map::new("hashtag_rel");
+
+pub const CHILD_RELATIONSHIP: Map<(u32, u32), bool> = Map::new("child_rel");
+
 pub const CALLOUT_NODE_RELATIONSHIP: Map<(&String, u32), bool> = Map::new("callout_rel");
-pub const NODE_HASHTAG_RELATIONSHIP: Map<(u32, &String), bool> = Map::new("node_hashtag_rel");
 pub const NODE_CALLOUT_RELATIONSHIP: Map<(u32, &String), bool> = Map::new("node_callout_rel");
+
+pub const HASHTAG_NODE_RELATIONSHIP: Map<(&String, u32), bool> = Map::new("hashtag_rel");
+pub const NODE_HASHTAG_RELATIONSHIP: Map<(u32, &String), bool> = Map::new("node_hashtag_rel");
